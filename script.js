ea0f1ab827be29ee84de55987f69802f6322face
Arkanafarel <arkanafarel811@gmail.com>
2026-03-04 11:17:11 +0700

    update

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-const video = document.getElementById("video");
+// const video = document.getElementById("video");
 
-navigator.mediaDevices.getUserMedia({ video: true })
-.then(stream => {
-    video.srcObject = stream;
-})
-.catch(error => {
-    alert("Camera tidak bisa diakses!");
-});
+// navigator.mediaDevices.getUserMedia({ video: true })
+// .then(stream => {
+//     video.srcObject = stream;
+// })
+// .catch(error => {
+//     alert("Camera tidak bisa diakses!");
+// });
 
-const button = document.getElementById("button-camera");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+// const button = document.getElementById("button-camera");
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
 
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+//     canvas.width = video.videoWidth;
+//     canvas.height = video.videoHeight;
 
-    ctx.drawImage(video, 0, 0);
-});
+//     ctx.drawImage(video, 0, 0);
+// });
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
 
-    canvas.width = 500;
-    canvas.height = 600;
+//     canvas.width = 500;
+//     canvas.height = 600;
 
-    // background putih
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+//     // background putih
+//     ctx.fillStyle = "white";
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // ukuran foto di tengah
-    let photoWidth = 400;
-    let photoHeight = 400;
+//     // ukuran foto di tengah
+//     let photoWidth = 400;
+//     let photoHeight = 400;
 
-    let x = (canvas.width - photoWidth) / 2;
-    let y = 50;
+//     let x = (canvas.width - photoWidth) / 2;
+//     let y = 50;
 
-    ctx.drawImage(video, x, y, photoWidth, photoHeight);
-});
+//     ctx.drawImage(video, x, y, photoWidth, photoHeight);
+// });
 
-const download = document.getElementById("download");
+// const download = document.getElementById("download");
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
 
-    canvas.width = 500;
-    canvas.height = 600;
+//     canvas.width = 500;
+//     canvas.height = 600;
 
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+//     ctx.fillStyle = "white";
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    let photoWidth = 400;
-    let photoHeight = 400;
+//     let photoWidth = 400;
+//     let photoHeight = 400;
 
-    let x = (canvas.width - photoWidth) / 2;
-    let y = 50;
+//     let x = (canvas.width - photoWidth) / 2;
+//     let y = 50;
 
-    ctx.drawImage(video, x, y, photoWidth, photoHeight);
+//     ctx.drawImage(video, x, y, photoWidth, photoHeight);
 
-    let image = canvas.toDataURL("image/png");
+//     let image = canvas.toDataURL("image/png");
 
-    download.href = image;
-    download.style.display = "block";
-});
+//     download.href = image;
+//     download.style.display = "block";
+// });
